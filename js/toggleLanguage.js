b427aed381793ab6431b4ec53139d7db31eab2a0
nuevo-rama <ramiromelero@gmail.com>
2023-10-23 00:01:49 -0300

    agrego ciclo para recorrer todos los key de data

--- js/toggleLanguage.js
+++ js/toggleLanguage.js
@@ -19,29 +19,11 @@ function loadContent(language) {
   fetch('./data/' + language)
     .then(response => response.json())
     .then(data => {
-      document.getElementById("heading").innerHTML = data.heading;
-      document.getElementById("content").innerHTML = data.content;
-      document.getElementById("menuItem1").innerHTML = data.menuItem1;
-      document.getElementById("menuItem2").innerHTML = data.menuItem2;
-      document.getElementById("menuItem3").innerHTML = data.menuItem3;
-      document.getElementById("title").innerHTML = data.title;
-      document.getElementById("subtitle").innerHTML = data.subtitle;
-      document.getElementById("textItem1").innerHTML = data.textItem1;
-
-      document.getElementById("sectionItem1").innerHTML = data.sectionItem1;
-      document.getElementById("sectionItem2").innerHTML = data.sectionItem2;
-      document.getElementById("sectionItem3").innerHTML = data.sectionItem3;
-
-      document.getElementById("step1").innerHTML = data.textItem2.step1;
-      document.getElementById("step2").innerHTML = data.textItem2.step2;
-      document.getElementById("step3").innerHTML = data.textItem2.step3;
-      document.getElementById("step4").innerHTML = data.textItem2.step4;
-      document.getElementById("step5").innerHTML = data.textItem2.step5;
-      document.getElementById("step6").innerHTML = data.textItem2.step6;
-      document.getElementById("step7").innerHTML = data.textItem2.step7;
-      document.getElementById("step8").innerHTML = data.textItem2.step8;
-      
-      
+      for (let key in data) {
+        if (document.getElementById(key)) {
+          document.getElementById(key).innerHTML = data[key];
+        }
+      }
     })
     .catch(error => console.log('Error al cargar los datos:', error));
 }
